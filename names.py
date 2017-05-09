67800b715aaab4f699148250c573ce17ef4fdcc5
Vishos <graham.layman@comcast.net>
2017-05-09 15:53:55 -0700

    adding names assignment

---
+++ names.py
@@ -0,0 +1,35 @@
+#Part I
+def users(arr):
+    for item in arr:
+        print item["first_name"], item["last_name"]
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+users(students)
+
+#Part II
+
+def users2(dict):
+    for key in dict:
+        print key
+        for x, item in enumerate(dict[key]):
+            print (x+1), "-", item["first_name"], item["last_name"], (len(item["first_name"])+len(item["last_name"]))
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+users2(users)
